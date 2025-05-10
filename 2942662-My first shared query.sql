7fa76ee0131141a5495c984f3b94fca79e644f87
Lakshay Solanki <lakshaysolanki60@gmail.com>
2025-05-10 08:21:00 +0000

    Update query `My first shared query`

--- 2942662-My first shared query.sql
+++ 2942662-My first shared query.sql
@@ -4,8 +4,8 @@ use sequal;
 show tables;
 INSERT into student(st_id,name,major)
 VALUES
-(7,'alice','mathematic'),
-(8,'Bob','QuantmPhysics'),
+(10,'alice','mathematic'),
+(11,'Bob','QuantmPhysics'),
 (9,'Charlie','Finance');
 
 select*from student;
