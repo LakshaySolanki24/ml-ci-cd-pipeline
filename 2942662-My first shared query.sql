cb608b180d73bffed9aed8e11dc884907b04b80f
Lakshay Solanki <lakshaysolanki60@gmail.com>
2025-05-10 07:28:21 +0000

    Update query `My first shared query`

--- 2942662-My first shared query.sql
+++ 2942662-My first shared query.sql
@@ -1,7 +1,6 @@
 show databases;
 use sequal;
 show tables;
-select*from student;
-select*from students;
-drop table students;
-show
+-- select*from student;
+-- select*from students;
+-- drop table students;
